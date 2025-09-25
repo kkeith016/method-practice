884e5d40d04ff7a3d18d4e16f7263b43cbe552f2
Kylee <kkeith.biz@gmail.com>
2025-09-25 13:47:05 -0400

    Positive Bools

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -1,12 +1,23 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+
 public class CompareNumbers {
     public static void main(String[] args) {
         System.out.println(isEven(5));
-        System.out.print(isEven(8));
+        System.out.println(isEven(8));
+
+        // Test isPositive method
+        System.out.println(isPositive(-3.5));
+        System.out.println(isPositive(2.0));
     }
 
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
+
+    public static boolean isPositive(double number) {
+        return number > 0;
+    }
+
 }
